51a3f990b2dd334cddd3e85bf9c844e88e57b9a6
Lucas Berthet <lu.berthet@gmail.com>
2026-05-25 19:03:54 +0200

    clean llmservice

--- src/llm/llm.types.ts
+++ src/llm/llm.types.ts
@@ -1,8 +1,3 @@
-export const SYSTEM_PROMPT = `Tu es un assistant. Tu vas recevoir des questions concernant des marques de produits. Ton travail est de répondre à la question à l'aide d'un tableau de string, sans aucun autre texte autour sous la forme de ['Marque 1', 'Marque 2', etc].`;
-
-export const OPENROUTER_API_URL =
-  'https://openrouter.ai/api/v1/chat/completions';
-
 export interface ChatMessage {
   role: 'system' | 'user' | 'assistant';
   content: string;
