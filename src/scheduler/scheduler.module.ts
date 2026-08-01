b4c36574426178883e4179f5f99063f9e88aa8d1
Lucas Berthet <lucas.berthet@kolecto.fr>
2026-08-01 15:58:37 +0200

    handle email service in the cron

--- src/scheduler/scheduler.module.ts
+++ src/scheduler/scheduler.module.ts
@@ -3,9 +3,11 @@ import { SchedulerService } from './scheduler.service';
 import { UsersModule } from 'src/users/users.module';
 import { RankingModule } from 'src/ranking/ranking.module';
 import { LlmModule } from 'src/llm/llm.module';
+import { GeoModule } from 'src/geo/geo.module';
+import { EmailModule } from 'src/email/email.module';
 
 @Module({
-  imports: [UsersModule, RankingModule, LlmModule],
+  imports: [UsersModule, RankingModule, LlmModule, GeoModule, EmailModule],
   providers: [SchedulerService],
 })
 export class SchedulerModule {}
